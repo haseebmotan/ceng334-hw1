e10e54c6ee81bb222df214f8c997d5f4a33f84c7
haseebmotan <haseebmotan07@gmail.com>
2023-04-12 15:02:55 +0300

    Fixed last loop for remaining bombers to print their explosions

--- main.c
+++ main.c
@@ -164,8 +164,6 @@ int main(int argc, char *argv[])
 
                     print_output(&in_message_print, NULL, NULL, NULL);
 
-                    om out_message;
-
                     if (in_message.type == BOMB_EXPLODE)
                     {
                         // Deactivate bomb
@@ -585,10 +583,40 @@ int main(int argc, char *argv[])
         usleep(1000);
     }
 
-    while (bombs_active_count--)
+    while (bombs_active_count)
     {
-        int child_status;
-        wait(&child_status);
+        for (int i = 0; i < bomb_count; i++)
+        {
+            if (!bombs_active[i])
+            {
+                continue;
+            }
+            else
+            {
+                struct pollfd in = {bomb_fds[i][0], POLLIN};
+                if (poll(&in, 1, 0) > 0)
+                {
+                    im in_message;
+                    read_data(bomb_fds[i][0], &in_message);
+
+                    imp in_message_print = {bomb_pids[i], &in_message};
+
+                    print_output(&in_message_print, NULL, NULL, NULL);
+
+                    if (in_message.type == BOMB_EXPLODE)
+                    {
+                        // Deactivate bomb
+                        bombs_active[i] = 0;
+                        bombs_active_count--;
+
+                        int child_status;
+                        waitpid(bomb_pids[i], &child_status, 0);
+
+                        close(bomb_fds[i][0]);
+                    }      
+                }
+            }  
+        }
     }
 
 }
